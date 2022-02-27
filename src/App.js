5ca720da152f5c193c4e4ab0a489a10e3b9d810a
Josep Conejero <bermeu@gmail.com>
2022-02-27 18:05:06 +0100

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import LoginPage from "./components/pages/LoginPage";
+
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import RegisterPage from "./components/pages/RegisterPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<LoginPage />}></Route>
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
